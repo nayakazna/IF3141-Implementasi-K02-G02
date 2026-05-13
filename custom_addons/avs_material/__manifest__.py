b5007e7ef487f39b3a2db84a82f411fdf52cc85b
azfaradhi <aradhihakim@gmail.com>
2026-05-13 10:46:39 +0700

    fix: manifest

--- custom_addons/avs_material/__manifest__.py
+++ custom_addons/avs_material/__manifest__.py
@@ -21,7 +21,7 @@
         "views/avs_procurement_views.xml",
     ],
     "installable": True,
-    "application": True,
+    "application": False,
     "auto_install": False,
     "license": "LGPL-3",
 }
